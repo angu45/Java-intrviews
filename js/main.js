e3e045a3973c599a5f2dbb9e981ca396e432f3b1
Angu <154960707+angu45@users.noreply.github.com>
2026-04-10 14:09:05 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,66 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  const toggleBtn = document.getElementById("theme-toggle");
-  const body = document.body;
+  fetch("components/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
 
-  if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-mode");
-    toggleBtn.textContent = "☀️";
-  }
+      initHeaderFeatures();
+    });
 
-  toggleBtn.addEventListener("click", function () {
-    body.classList.toggle("dark-mode");
+  fetch("components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
 
-    if (body.classList.contains("dark-mode")) {
-      localStorage.setItem("theme", "dark");
-      toggleBtn.textContent = "☀️";
-    } else {
-      localStorage.setItem("theme", "light");
-      toggleBtn.textContent = "🌙";
+  function initHeaderFeatures() {
+
+    const menuBtn = document.getElementById("menu-btn");
+    const navMenu = document.getElementById("nav-menu");
+    const toggleBtn = document.getElementById("theme-toggle");
+    const body = document.body;
+    const searchBtn = document.getElementById("search-btn");
+    const searchInput = document.getElementById("searchInput");
+
+    if (menuBtn) {
+      menuBtn.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+      });
     }
-  });
+
+    if (localStorage.getItem("theme") === "dark") {
+      body.classList.add("dark-mode");
+      if (toggleBtn) toggleBtn.textContent = "☀️";
+    }
+
+    if (toggleBtn) {
+      toggleBtn.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+          localStorage.setItem("theme", "dark");
+          toggleBtn.textContent = "☀️";
+        } else {
+          localStorage.setItem("theme", "light");
+          toggleBtn.textContent = "🌙";
+        }
+      });
+    }
+
+    if (searchBtn && searchInput) {
+      searchBtn.addEventListener("click", function () {
+        searchInput.classList.toggle("active");
+        searchInput.focus();
+      });
+
+      document.addEventListener("click", function (e) {
+        if (!searchBtn.contains(e.target) && !searchInput.contains(e.target)) {
+          searchInput.classList.remove("active");
+        }
+      });
+    }
+
+  }
 
 });
